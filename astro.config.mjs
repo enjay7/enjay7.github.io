7a6398aac6feb85fea62df1e017a106255e90e01
enjay7 <102714508+enjay7@users.noreply.github.com>
2024-05-11 02:38:43 -0700

    Initial astro home page commit

---
+++ astro.config.mjs
@@ -0,0 +1,38 @@
+import { defineConfig } from 'astro/config'
+
+import svelte from '@astrojs/svelte'
+import tailwind from '@astrojs/tailwind'
+import sitemap from '@astrojs/sitemap'
+import mdx from '@astrojs/mdx'
+import compress from 'astro-compress'
+import rehypeSlug from 'rehype-slug'
+import rehypeAutoLinkHeadings from 'rehype-autolink-headings'
+import rehypeCodeTitles from 'rehype-code-titles'
+import rehypePrism from 'rehype-prism'
+
+export default defineConfig({
+  site: 'https://naveenjetty.com',
+	markdown: {
+		syntaxHighlight: false,
+		rehypePlugins: [
+			rehypeSlug,
+			rehypeAutoLinkHeadings,
+			rehypeCodeTitles,
+			rehypePrism,
+		],
+  },
+  integrations: [
+    svelte(),
+		tailwind({ config: { applyBaseStyles: false } }),
+		mdx(),
+		sitemap(),
+		compress(),
+	],
+	vite: {
+		server: {
+			proxy: {
+				'/api': 'http://localhost:8787',
+			},
+		},
+	},
+})
---
+++ src/pages/feed.xml.ts
@@ -0,0 +1,20 @@
+import rss from '@astrojs/rss'
+
+
+export async function GET(context: any) {
+  const posts = await import.meta.glob('./blog/*.md');
+  const items = await Promise.all(Object.values(posts).map(async (post) => await post()));
+
+  console.log(items)
+	return rss({
+		title: `Naveen's Blog`,
+		description: "A humble Astronaut's guide to the stars",
+		site: context.site,
+		items: items.map((post) => ({
+      link: `/blog/${post.slug}/`,
+      title: post.frontmatter.title,
+      description: post.frontmatter.summary,
+      pubDate: new Date(Date.parse(post.frontmatter.publishedAt)),
+    }))
+	})
+}
